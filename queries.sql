d6a3775e2604ef67d21390f1efec46d8d5f57b43
Sylvestre <pasytchangwa@gmail.com>
2021-09-27 18:59:44 +0300

    Correct typo on name

--- queries.sql
+++ queries.sql
@@ -2,9 +2,9 @@
 
 SELECT * FROM animals WHERE Name LIKE '%mon%';
 SELECT Name FROM animals WHERE Date_of_birth BETWEEN (to_date('01/01/2016','mm/dd/yyyy')) AND (to_date('12/31/2019','mm/dd/yyyy'));
-SELECT Name FROM animals WHERE Neutered = true AND Escape_attempts < 3;
-SELECT Date_of_birth FROM animals WHERE Name = 'Agumon' or name= 'Pikachu';
+SELECT Name FROM animals WHERE Neutered = True AND Escape_attempts < 3;
+SELECT Date_of_birth FROM animals WHERE Name = 'Agumon' or Name= 'Pikachu';
 SELECT Name,Escape_attempts FROM animals WHERE Weight_kg > 10.5;
-SELECT * FROM animals WHERE Neutered = true;
+SELECT * FROM animals WHERE Neutered = True;
 SELECT * FROM animals WHERE Name != 'Gabumon';
 SELECT * FROM animals WHERE Weight_kg >= 10.4 AND Weight_kg <= 17.3;
