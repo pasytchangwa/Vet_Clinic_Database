f9a5619030329209c8399aa17a9c5401e44bc5c9
Sylvestre <pasytchangwa@gmail.com>
2021-09-27 17:29:04 +0300

    Insert data into table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (1, 'Agumon', (to_date('02/03/2020','mm/dd/yyyy')), 0, True, 10.23 );
+INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (2, 'Gabumon', (to_date('11/15/2018','mm/dd/yyyy')), 2, True, 8 );
+INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (3, 'Pikachu', (to_date('01/07/2021','mm/dd/yyyy')), 1, False, 15.04 );
+INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (4, 'Devimon', (to_date('05/12/2017','mm/dd/yyyy')), 5, True, 11 );
