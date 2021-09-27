5492b0e3f4541ccf8945876c4356c482c5589849
Sylvestre <pasytchangwa@gmail.com>
2021-09-27 18:52:57 +0300

    Make id to generate automatically

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    Id integer PRIMARY KEY NOT NULL,
+    Id integer GENERATED ALWAYS AS IDENTITY,
     Name varchar(100),
     Date_of_birth DATE,
     Escape_attempts INT,
     Neutered BOOLEAN,
-    Weight_kg DECIMAL
+    Weight_kg DECIMAL,
+    PRIMARY KEY(Id)
 );
