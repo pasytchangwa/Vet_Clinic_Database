e7fc894c894781dff15cc7d5fe1abedf2875bff6
Sylvestre <pasytchangwa@gmail.com>
2021-09-27 18:53:18 +0300

    Remove ID from insertions

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (1, 'Agumon', (to_date('02/03/2020','mm/dd/yyyy')), 0, True, 10.23 );
-INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (2, 'Gabumon', (to_date('11/15/2018','mm/dd/yyyy')), 2, True, 8 );
-INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (3, 'Pikachu', (to_date('01/07/2021','mm/dd/yyyy')), 1, False, 15.04 );
-INSERT INTO animals (Id,Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES (4, 'Devimon', (to_date('05/12/2017','mm/dd/yyyy')), 5, True, 11 );
+INSERT INTO animals (Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES ('Agumon', (to_date('02/03/2020','mm/dd/yyyy')), 0, True, 10.23 );
+INSERT INTO animals (Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES ('Gabumon', (to_date('11/15/2018','mm/dd/yyyy')), 2, True, 8 );
+INSERT INTO animals (Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES ('Pikachu', (to_date('01/07/2021','mm/dd/yyyy')), 1, False, 15.04 );
+INSERT INTO animals (Name,Date_of_birth,Escape_attempts,Neutered,Weight_kg) VALUES ('Devimon', (to_date('05/12/2017','mm/dd/yyyy')), 5, True, 11 );
